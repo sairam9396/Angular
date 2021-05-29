9de5785484a124be0dc2897ec13603cf3833953e
Sairam <sairamus96@outlook.com>
2021-05-30 00:58:48 +0530

    Servers component added.

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['../app.component.css']
+})
+
+export class ServerComponent {
+    name: String = 'Server Component';
+}
